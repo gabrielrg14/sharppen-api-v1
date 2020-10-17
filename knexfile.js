182220fd262f813afcfe369042febf6b6598412e
Gabriel Rapucci <gabrielrapuccigonzalez14@hotmail.com>
2020-10-17 17:10:57 -0300

    Fixed issue on Deploying Heroku

--- knexfile.js
+++ knexfile.js
@@ -1,19 +1,19 @@
-// const { db } = require('./.env')
+const { db } = require('./.env')
 
 module.exports = {
-    // development: {
-    //     client: 'postgresql',
-    //     connection: db,
-    //     pool: {
-    //         min: 2,
-    //         max: 10
-    //     },
-    //     migrations: {
-    //         tableName: 'knex_migrations'
-    //     }
-    // },
-    production: {
+    development: {
         client: 'postgresql',
+        connection: db,
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: 'knex_migrations'
+        }
+    },
+    production: {
+        client: 'pg',
         connection: process.env.DATABASE_URL,
         migrations: {
             directory: __dirname + '/migrations',
