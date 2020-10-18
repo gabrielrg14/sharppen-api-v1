5b6ff8e551a4c565e9de71bc04e6f467cfdee4aa
Gabriel Rapucci <gabrielrapuccigonzalez14@hotmail.com>
2020-10-18 15:29:41 -0300

    Fixed issue on Deploying Heroku

--- config/db.js
+++ config/db.js
@@ -1,8 +1,5 @@
 const config = require('../knexfile.js')
-const env = process.env.DB_ENV || 'development'
-const knex = require('knex')(config[env])
+const knex = require('knex')(config)
 
-
-
-// knex.migrate.latest([config])
+knex.migrate.latest([config])
 module.exports = knex
