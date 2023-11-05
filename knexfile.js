3f7f4db2203eed590a902c01916deb0605270988
Gabriel Rapucci <gabrielrapuccigonzalez14@hotmail.com>
2023-11-05 13:01:45 -0300

    Add ssl=true on POSTGRES_URL parameter

--- knexfile.js
+++ knexfile.js
@@ -3,12 +3,7 @@ require('dotenv').config()
 module.exports = {
     development: {
         client: 'pg',
-        connection: {
-            host: process.env.POSTGRES_HOST,
-            port: process.env.POSTGRES_PORT,
-            user: process.env.POSTGRES_USER,
-            password: process.env.POSTGRES_PASSWORD
-        },
+        connection: process.env.POSTGRES_URL,
         pool: {
             min: 2,
             max: 10
@@ -19,12 +14,7 @@ module.exports = {
     },
     production: {
         client: 'pg',
-        connection: {
-            host: process.env.POSTGRES_HOST,
-            port: process.env.POSTGRES_PORT,
-            user: process.env.POSTGRES_USER,
-            password: process.env.POSTGRES_PASSWORD
-        },
+        connection: process.env.POSTGRES_URL + "?ssl=true",
         pool: {
             min: 2,
             max: 10
